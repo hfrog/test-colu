9320f4db086022d06b255d75ecf0060aa88e0b73
hfrog <and@hfrog.ru>
2015-12-15 08:19:16 +0000

    Added config file via app-config module

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@
 //
 
 var express = require('express');
+var config = require('app-config');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var apiKey = config.colu.apiKey;
+  var privateSeed = config.colu.privateSeed;
+  res.render('index', { title: 'Express', apiKey: apiKey, privateSeed: privateSeed });
 });
 
 module.exports = router;
